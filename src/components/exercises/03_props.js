b4675dd745531b29c48310a6f63739ce9421b2b0
Abid Ali <studytimee@gmail.com>
2022-03-25 16:33:36 +0000

    Exercise 3 Props- Add modules tables

--- src/components/exercises/03_props.js
+++ src/components/exercises/03_props.js
@@ -2,9 +2,33 @@ import React from "react";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
 
 
-const Demo = () => {
+const Demo = (props) => {
   return (
-      <h1>TODO</h1>
+    <div>
+    <h1>{`${course} - Module tables`}</h1>
+    <table className="table table-bordered">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>No lectures</th>
+          <th>No practicals</th>
+        </tr>
+      </thead>
+      <tbody >
+        <tr>
+          <td>{module[0]}</td>
+          <td>2</td>
+          <td>3</td>
+        </tr>
+        <tr>
+          <td>{module[1]}</td>
+          <td>2</td>
+          <td>2</td>
+        </tr>
+      </tbody >
+    </table>
+  </div>
+
   );
 };
 
