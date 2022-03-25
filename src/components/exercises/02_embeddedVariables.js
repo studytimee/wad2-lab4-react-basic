d36b22f25e77c1abd2519ecb1fb07402929905db
Abid Ali <studytimee@gmail.com>
2022-03-25 07:24:57 +0000

    Exercise 2 - Embedded Variables Completed.

--- src/components/exercises/02_embeddedVariables.js
+++ src/components/exercises/02_embeddedVariables.js
@@ -1,10 +1,34 @@
 import React from "react";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
-
-
 const Demo = () => {
+  const course = "MSC Enterprise Software Systems";
+  const module = ["Cloud Architecture", "Enterprise Web Dev"];
+
   return (
-      <h1>TODO</h1>
+    <div>
+      <h1>{`${course} - Module tables`}</h1>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>No lectures</th>
+            <th>No practicals</th>
+          </tr>
+        </thead>
+        <tbody >
+          <tr>
+            <td>{module[0]}</td>
+            <td>2</td>
+            <td>3</td>
+          </tr>
+          <tr>
+            <td>{module[1]}</td>
+            <td>2</td>
+            <td>2</td>
+          </tr>
+        </tbody >
+      </table>
+    </div>
   );
 };
 
